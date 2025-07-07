d6ff5fd89dc223bb96a388f456c0e8281cd08d03
Olawale Babalola <103125997+Babalolaolawale@users.noreply.github.com>
2025-07-07 21:40:25 +0100

    font change

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -94,7 +94,7 @@ function Home() {
             />
           </div>
           <p
-            className={`font-sans text-base md:text-lg mb-2 ${
+            className={`font-inter text-base md:text-lg mb-2 ${
               isDarkMode ? "text-gray-300" : "text-gray-700"
             }`}
           >
@@ -105,7 +105,7 @@ function Home() {
             real-world complexities of the environments we serve.
           </p>
           <p
-            className={`font-sans text-base md:text-lg mb-4 ${
+            className={`font-inter text-base md:text-lg mb-4 ${
               isDarkMode ? "text-gray-300" : "text-gray-700"
             }`}>
             While crafting compelling experiences, zuri.corp serves as the
