7a0899cb5b1489bd156b8a60b1dc0b742f37a7d2
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2025-07-17 22:12:49 +0100

    problem with postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				// Use hsl to interpolate custom properties
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: "hsl(var(--card))",
+				"card-foreground": "hsl(var(--card-foreground))",
+				// ... Repeat for any other vars you need:
+				primary: "hsl(var(--primary))",
+				"primary-foreground": "hsl(var(--primary-foreground))",
+			},
+		},
+	},
+	plugins: [],
 };
